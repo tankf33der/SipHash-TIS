5946b0493bd7f84709c7b3e0faee94c6737e6761
Mike <tankf33der@disroot.org>
2021-09-19 09:28:04 +0300

    .

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 all:
-	gcc *.c
+	gcc *.c && ./a.out
+a:
+	clang -fsanitize=address,undefined *.c && ./a.out
+m:
+	clang -fsanitize=memory *.c && ./a.out
 c:
 	rm -rf *.o *.out
 	
--- main.c
+++ main.c
@@ -2,6 +2,33 @@
 #include "halfsiphash.h"
 #include "siphash.h"
 
+typedef uint8_t u8;
+
+#define ARRAY(name, size) \
+    u8 name[size]; \
+    for(size_t i = 0; i < size; i++) name[i] = i;
+
+void sip(void) {
+	ARRAY(in, 64);
+	ARRAY(out, 64);
+	ARRAY(key, 16);
+
+	for(size_t i = 0; i < 64; i++)
+		siphash(in, i, key, out, 8);
+}
+
+void half(void) {
+	ARRAY(in, 64);
+	ARRAY(out, 64);
+	ARRAY(key, 16);
+
+	for(size_t i = 0; i < 64; i++)
+		halfsiphash(in, i, key, out, 8);
+}
+
+
 int main(void) {
+	sip();
+	half();
 	return 0;
 }
